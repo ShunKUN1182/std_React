7eb182a28184ae40e0648cc2c815d9b807c3ba8a
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-13 19:42:42 +0900

    フラグメントのお勉強

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,17 @@
+import { Fragment } from "react";
 import "./Child.css";
+
+
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <Fragment key="">
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae error repudiandae molestiae eius perspiciatis similique quo aspernatur tempore corrupti alias minus delectus, dolor earum praesentium nam pariatur voluptatibus sunt accusamus?</p>
+    </Fragment>
   );
 };
 
