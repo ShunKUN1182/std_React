9380dcc8f35e4e76df61f8c8238540d5fb9dcbfd
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-17 01:31:39 +0900

    propsのお勉強

--- 04_react_basic/src/090_props/start/components/Child.jsx
+++ 04_react_basic/src/090_props/start/components/Child.jsx
@@ -5,12 +5,12 @@
 
 import "./Child.css";
 
-const Child = () => {
-  return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
-  );
+const Child = ({ color = "green" }) => {
+    return (
+        <div className={`component ${color}`}>
+            <h3>Hello Component</h3>
+        </div>
+    );
 };
 
 export default Child;
