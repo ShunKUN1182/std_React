b0f0512d40db3a5a255943e8e895523070bcf19c
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-28 09:39:08 +0900

    テストアップロード

--- 05_state_and_event/src/030_useState_render/start/Example.jsx
+++ 05_state_and_event/src/030_useState_render/start/Example.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
 
 const Example = () => {
-  let displayVal;
-  let [ val, setVal ] = useState();
-  console.log('再レンダリングされました');
-  return (
-    <>
-      <input
-        type="text"
-        onChange={(e) => {
-          console.log(e.target.value);
-          setVal(e.target.value);
-          // displayVal = e.target.value;
-        }}
-      />
-      = {val}
-    </>
-  );
+    let displayVal;
+    let [val, setVal] = useState();
+    console.log("再レンダリングされました");
+    return (
+        <>
+            <p>aaaaa</p>
+            <input
+                type="text"
+                onChange={(e) => {
+                    console.log(e.target.value);
+                    setVal(e.target.value);
+                    // displayVal = e.target.value;
+                }}
+            />
+            = {val}
+        </>
+    );
 };
 
 export default Example;
