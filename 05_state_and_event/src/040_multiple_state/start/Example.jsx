43549a29dbfd641abe1d8ea8232e04dbe82ff1b1
ShunKUN1182 <2250378@ecc.ac.jp>
2025-12-01 18:46:48 +0900

    040終了

--- 05_state_and_event/src/040_multiple_state/start/Example.jsx
+++ 05_state_and_event/src/040_multiple_state/start/Example.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
 const Example = () => {
-  
+    const [countA, setCountA] = useState(0);
+    const [countB, setCountB] = useState(10);
+    const [countC, setCountC] = useState(100);
+    return (
+        <>
+            <p>ボタンAを{countA}回押しました！</p>
+            <button
+                onClick={() => {
+                    setCountA(countA + 1);
+                }}
+            >
+                countAぼたん
+            </button>
+            <p>ボタンBを{countB}回押しました！</p>
+            <button
+                onClick={() => {
+                    setCountB(countB + 1);
+                }}
+            >
+                countBぼたん
+            </button>
+            <p>ボタンCを{countC}回押しました！</p>
+            <button
+                onClick={() => {
+                    setCountC(countC + 1);
+                }}
+            >
+                countCぼたん
+            </button>
+        </>
+    );
 };
 
 export default Example;
