73cfc4aa5cf465799b036516d84a2d8aa83ad784
ShunKUN1182 <2250378@ecc.ac.jp>
2025-12-04 22:37:07 +0900

    060終了

--- 05_state_and_event/src/060_state_object/start/Example.jsx
+++ 05_state_and_event/src/060_state_object/start/Example.jsx
@@ -1,7 +1,31 @@
 import { useState } from "react";
 
 const Example = () => {
-  const personObj = { name: "Tom", age: 18 };
+    const personObj = { name: "Tom", age: 18 };
+    const [person, setPerson] = useState(personObj);
+
+    const changeName = (e) => {
+        setPerson({ name: e.target.value, age: person.age });
+    };
+    const changeAge = (e) => {
+        setPerson({ name: person.name, age: e.target.value });
+    };
+    const reset = () => {
+        setPerson({ name: "", age: "" });
+    };
+    return (
+        <>
+            <h3>Name:{person.name}</h3>
+            <h3>Age:{person.age}</h3>
+            <input type="text" onChange={changeName} value={person.name} />
+            <input type="number" onChange={changeAge} value={person.age} />
+            <div>
+                <button type="button" onClick={reset}>
+                    リセット
+                </button>
+            </div>
+        </>
+    );
 };
 
 export default Example;
