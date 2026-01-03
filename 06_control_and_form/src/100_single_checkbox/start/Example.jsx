521cdbd7e1930e604f7c9727126b5cd01d336da5
ShunKUN1182 <2250378@ecc.ac.jp>
2026-01-04 03:22:48 +0900

    100終了

--- 06_control_and_form/src/100_single_checkbox/start/Example.jsx
+++ 06_control_and_form/src/100_single_checkbox/start/Example.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 const Example = () => {
-  return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
-  );
+    const [isChecked, setisChecked] = useState(true);
+    const toggleChecked = (e) => {
+        setisChecked((prevState) => !prevState);
+    };
+    return (
+        <div>
+            <label htmlFor="my-check">チェック：</label>
+            <input
+                type="checkbox"
+                id="my-check"
+                checked={isChecked}
+                onChange={() => toggleChecked()}
+            />
+            <div>{isChecked ? "ON" : "OFF"}</div>
+        </div>
+    );
 };
 
 export default Example;
