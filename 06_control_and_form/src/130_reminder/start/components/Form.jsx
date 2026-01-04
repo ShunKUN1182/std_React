abb3624985aad41e76b74bdff2b6d81201d138b5
ShunKUN1182 <2250378@ecc.ac.jp>
2026-01-04 14:05:21 +0900

    9割完成

--- 06_control_and_form/src/130_reminder/start/components/Form.jsx
+++ 06_control_and_form/src/130_reminder/start/components/Form.jsx
@@ -1,5 +1,23 @@
-const Form = () => {
-    return <></>;
+import { useState } from "react";
+
+const Form = ({ createTodo }) => {
+    const [entaredTodo, setEntaredTodo] = useState("");
+    const changeTodo = (e) => {
+        setEntaredTodo(e.target.value);
+    };
+    const addTodo = (e) => {
+        const newTodo = {
+            id: Math.floor(Math.random() * 1e5),
+            content: entaredTodo,
+        };
+        createTodo(newTodo);
+    };
+    return (
+        <div>
+            <input type="text" value={entaredTodo} onChange={(e) => changeTodo(e)} />
+            <button onClick={addTodo}>追加</button>
+        </div>
+    );
 };
 
 export default Form;
